b8d5f28b1c471c2185d21409ce31254e10272e0a
Nevertheless <swgu931@chol.com>
2020-08-06 11:34:25 +0900

    Update gke-create-nav2.sh

--- gke-create-nav2.sh
+++ gke-create-nav2.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-gcloud config set project lg-cloud-robot-20200731     # project in accountn@googlemail.com
+export my-gcp-project = "my-gcp-project"
+export my-gke-cluster = "my-gke-cluster"
+
+gcloud config set project $my-gcp-project     # project in accountn@googlemail.com
 gcloud config set compute/zone asia-northeast3   # Seoul region
 
-gcloud container clusters create nav2-cloud --project=lg-cloud-robot-20200731 --cluster-version=1.15.12-gke.2 \
+gcloud container clusters create nav2-cloud --project=$my-gcp-project  --cluster-version=1.15.12-gke.2 \
 --machine-type=n1-standard-1 \
 --num-nodes=1 \
 --subnetwork=default
 
-gcloud compute project-info describe --project lg-cloud-robot-20200731
+gcloud compute project-info describe --project $my-gcp-project 
 
-gcloud container clusters describe nav2-cloud
+gcloud container clusters describe $my-gke-cluster
 
-gcloud container clusters get-credentials nav2-cloud
+gcloud container clusters get-credentials $my-gke-cluster
 kubectl get nodes  
 
 kubectl apply -f https://raw.githubusercontent.com/kubetm/kubetm.github.io/master/sample/practice/appendix/gcp-kubernetes-dashboard.yaml 
-kubectl proxy --port=8002 &
+kubectl proxy --port=8001 &
 
-echo "please visit to GCP then disable stackdriver service in the cluster"
-echo "dashboard access to http://127.0.0.1:8002/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/#!/overview?namespace=default"
+# access to ashboard access to http://127.0.0.1:8002/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/#!/overview?namespace=default"
 
