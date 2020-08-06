4895df8052ec09b2e97e97889335a68788523cef
Nevertheless <swgu931@chol.com>
2020-08-06 11:35:21 +0900

    Update gke-delete-nav2.sh

--- gke-delete-nav2.sh
+++ gke-delete-nav2.sh
@@ -1,7 +1,8 @@
 !#/bin/bash
 
+export my-gcp-project = "my-gcp-project"
+export my-gke-cluster = "my-gke-cluster"
 
-
-gcloud container clusters delete nav2-cloud --project=lg-cloud-robot-20200731
+gcloud container clusters delete $my-gke-cluster --project=$my-gcp-project
 
 gcloud container clusters describe nav2-cloud
