3f72231fcbe25c87e0913c8c4f8127f4352e349c
sewan.gu <sewan.gu@lge.com>
2021-08-22 16:45:45 +0900

    Add something to change

--- gcp-access-docker-run.sh
+++ gcp-access-docker-run.sh
@@ -8,3 +8,16 @@ docker run -it --rm \
 -v ~/second/workspace-cloud/:/shared:rw \
 swgu931/google-cloud-sdk-kubespray-terraform \
 /bin/bash
+
+
+## please follow the command below
+# 
+# gcloud config set project lg-cloud-robot-20200730
+# gcloud config set compute/zone asia-northeast3
+# gcloud compute ssh --zone=asia-northeast3-a gce-k8s-master
+
+
+## then make sure that the value KUBECONFIG was properly set, otherwise run the command below
+# export KUBECONFIG=/etc/kubernetes/admin.conf
+
+
